9c05e04202465b2792eb2cfdcbdbb426e2ff5563
Herosan163 <efi1retteb@icloud.com>
2019-11-10 05:43:30 +0000

    ad

--- model.py
+++ model.py
@@ -22,6 +22,7 @@ class convolutional_auto_encoder(Model):
     def __call__(self, x, is_training):
         encoded_feature = self.encode(x)
         decoded_featrue = self.decode(x)
+        return encoded_feature, decoded_featrue
 
     def encode(self, x):
         x = self.conv1(x)
---
+++ train_cae.py
@@ -0,0 +1,34 @@
+import numpy as np
+import tensorflow as tf
+from model import convolutional_auto_encoder
+
+@tf.function
+def train_step(model, train_loss, optimizer, image, label):
+    with tf.GradientTape() as tape:
+        predictions = model(image)
+        loss = train_loss(label, predictions)
+    gradients = tape.gradient(loss, model.trainable_variables)
+    optimizer.apply_gradients(zip(gradients, model.trainable_variables))
+    train_loss(loss)
+
+
+if __name__ == '__main__':
+    model_appearance = convolutional_auto_encoder()
+    model_motion1 = convolutional_auto_encoder()
+    model_motion2 = convolutional_auto_encoder()
+    loss_object = tf.keras.losses.MSE()
+    optimizer = tf.keras.optimizers.Adam()
+    train_loss = tf.keras.metrics.Mean(name='train_loss')
+
+    EPOCHS = 5
+
+    for epoch in range(EPOCHS):
+        for image, label in train_ds:
+            train_step(image, label)
+
+    template = 'Epoch {}, Loss: {}, Accuracy: {}, Test Loss: {}, Test Accuracy: {}'
+    print (template.format(epoch+1,
+                           train_loss.result(),
+                           train_accuracy.result()*100,
+                           test_loss.result(),
+                           test_accuracy.result()*100))
