ce8bb8c3d660a9ddeedad8c4883370ab47b189dc
Herosan163 <efi1retteb@icloud.com>
2019-12-23 12:11:23 +0000

    model structure change

--- model.py
+++ model.py
@@ -3,27 +3,28 @@ from tensorflow.keras import Model
 from tensorflow.keras.layers import Dense, Flatten, Conv2D, MaxPooling2D, UpSampling2D
 
 class convolutional_auto_encoder(Model):
-    def __init__(self):
+    def __init__(self, name):
         super(convolutional_auto_encoder, self).__init__()
+        self.name_prefix = name
         self.encode_net = tf.keras.Sequential(
             [
-                Conv2D(32, 3, activation='relu', padding='SAME'),
-                MaxPooling2D(2, 2),
-                Conv2D(32, 3, activation='relu', padding='SAME'),
-                MaxPooling2D(2, 2),
-                Conv2D(16, 3, activation='relu', padding='SAME'),
-                MaxPooling2D(2, 2)
+                Conv2D(32, 3, activation='relu', padding='SAME', name=name+'_conv1'),
+                MaxPooling2D(2, 2, padding='SAME', name=name+'_'+'pool1'),
+                Conv2D(32, 3, activation='relu', padding='SAME', name=name+'_conv2'),
+                MaxPooling2D(2, 2, padding='SAME', name=name+'_'+'pool2'),
+                Conv2D(16, 3, activation='relu', padding='SAME', name=name+'_conv3'),
+                MaxPooling2D(2, 2, padding='SAME', name=name+'_pool3')
             ]
         )
         self.decode_net = tf.keras.Sequential(
             [
-                Conv2D(16, 3, activation='relu', padding='SAME'),
-                UpSampling2D(2, interpolation='nearest'),
-                Conv2D(32, 3, activation='relu', padding='SAME'),
-                UpSampling2D(2, interpolation='nearest'),
-                Conv2D(32, 3, activation='relu', padding='SAME'),
-                UpSampling2D(2, interpolation='nearest'),
-                Conv2D(1, 3, padding='SAME')
+                UpSampling2D(2, interpolation='nearest', name=name+'_upsampling1'),
+                Conv2D(16, 3, activation='relu', padding='SAME', name=name+'_conv4'),
+                UpSampling2D(2, interpolation='nearest', name=name+'_upsampling2'),
+                Conv2D(32, 3, activation='relu', padding='SAME', name=name+'_conv5'),
+                UpSampling2D(2, interpolation='nearest', name=name+'_upsampling3'),
+                Conv2D(32, 3, activation='relu', padding='SAME', name=name+'_conv6'),
+                Conv2D(1, 3, padding='SAME', name=name+'_conv7')
             ]
         )
 
